054febce80cdaae1bf29e08c135a2432aa8d18bb
Peter Daum <daum@tyntec.com>
2019-12-08 16:08:43 +0100

    started adding comments

--- src/main/kotlin/com/tyntec/ktor/problem/ProblemContext.kt
+++ src/main/kotlin/com/tyntec/ktor/problem/ProblemContext.kt
@@ -18,6 +18,9 @@ package com.tyntec.ktor.problem
 
 import io.ktor.application.ApplicationCall
 
+/**
+ * Container class available when exception based configuration is used.
+ */
 data class ProblemContext<T : Throwable>(
     val call: ApplicationCall,
     val throwable: T
